43179d2df9772542985e5c0a5c4f1a9ceaf669b9
Marina <marina.l@edu.senai.br>
2026-06-12 23:33:46 -0300

    Atualiza Sistema de Aprovação Escolar

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 let alunos = ["João", "Maria", "Pedro", "Ana"];
-let notas = [[8, 7, 9], [9, 8, 10], [7, 6, 8], [10, 9, 9]];
-let presenca = [true, false, true, true];
+let notas = [[8, 7, 9], [9, 8, 10], [7, 7.5, 8], [10, 9, 9.5]];
+let presenca = [[true, false, true, true], [true, false, true, false], [true, true, true, true], [true, true, true, false]];
 
-for (let i = 0; i < alunos.length; i++) {
-    let media = (notas[i][1] + notas[i][2] + notas[i][3])/3;
-};
+for (i = 0; i < alunos.length; i++) {
 
-//for (i = 0; i < alunos.length; i++) {
-//    console.log(alunos[i] + ": " + notas[i] + " - Presença: " + presenca[i]);
-//};
+    let somas = [0, 0, 0, 0];
+    for (i2 = 0; i2 < notas[i].length; i2++){
+        //i2: índice de uma array dentro de outra array
+        somas[i] += notas[i][i2];
+    }
+
+    let media = somas[i] / notas[i].length;
+    
+    let porcentagemPresenca = [0, 0, 0, 0];
+    for (i2 = 0; i2 < presenca[i].length; i2++){
+        if (presenca[i][i2] === true){
+            porcentagemPresenca[i] += (100 / presenca[i].length);
+            //porcentagem: valor em porcentagem de cada presença somado
+        }
+    }
+
+    console.log("A média de " + alunos[i] + " foi: " + media + ".");
+    console.log("Ele(a) obteve " + porcentagemPresenca[i] + "% de presença.");
+    if ((media >= 6) && (porcentagemPresenca[i] >= 75)){
+        console.log("Aprovado(a)!");
+    } else {
+        console.log("Reprovado.");
+    }
+    console.log();
+}
