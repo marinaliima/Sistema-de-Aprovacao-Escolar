4e99f473b3c166dbeba8fc86cb89f10758dda80e
Marina <marina.l@edu.senai.br>
2026-06-13 21:19:06 -0300

    Atualiza Sistema de Aprovação Escolar

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let alunos = ["João", "Maria", "Pedro", "Ana"];
-let notas = [[8, 7, 9], [9, 8, 10], [7, 7.5, 8], [10, 9, 9.5]];
+let notas = [[80, 70, 90], [90, 80, 100], [70, 70, 10], [100, 90, 95]];
 let presenca = [[true, false, true, true], [true, false, true, false], [true, true, true, true], [true, true, true, false]];
+let melhorMedia = 0;
+let melhorAluno = "";
 
 for (i = 0; i < alunos.length; i++) {
 
     let somas = [0, 0, 0, 0];
+    
     for (i2 = 0; i2 < notas[i].length; i2++){
         //i2: índice de uma array dentro de outra array
         somas[i] += notas[i][i2];
@@ -22,10 +25,24 @@ for (i = 0; i < alunos.length; i++) {
 
     console.log("A média de " + alunos[i] + " foi: " + media + ".");
     console.log("Ele(a) obteve " + porcentagemPresenca[i] + "% de presença.");
-    if ((media >= 6) && (porcentagemPresenca[i] >= 75)){
+
+    if ((media >= 70) && (porcentagemPresenca[i] >= 75)){
         console.log("Aprovado(a)!");
+    } else if ((media >= 50) && (porcentagemPresenca[i] >= 75)){
+        console.log("Recuperação.");
     } else {
-        console.log("Reprovado.");
+        console.log("Reprovado(a).");
     }
     console.log();
+
+    if (media > melhorMedia){
+        melhorMedia = media;
+        melhorAluno = alunos[i];
+    }
+
+    if (i === (alunos.length) - 1){
+        console.log("——————————————————————————————————————————————");
+        console.log();
+        console.log("O melhor desempenho foi de " + melhorAluno + ", com média " + melhorMedia + "!"); 
+    }
 }
